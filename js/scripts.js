bf2855be771a26838ed05f76e7a9dba0a62d5c56
Evan Nartea <evannartea@Mac.lan>
2026-07-03 18:09:29 +0100

    added functionality for prev/next buttons

--- js/scripts.js
+++ js/scripts.js
@@ -19,9 +19,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const type2Icon = document.getElementById("type2");
     const pokemonSprite = document.getElementById("pokemon-sprite");
     const pokemonCry = document.getElementById("pokemon-cry");
+
+    let currentId = null;
+    const MAX_POKEMON = 1025;
     
-    // Fetch data
-    async function getPokemon(query) {
+    // Fetch data from API
+    async function fetchData(query) {
         const url = `https://pokeapi.co/api/v2/pokemon/${query}`
 
         try {
@@ -31,7 +34,6 @@ document.addEventListener("DOMContentLoaded", function() {
             if(!response.ok) {
                 throw new Error("Could not fetch resource");
             }
-
             const data = await response.json();
             return data;
         }
@@ -39,48 +41,53 @@ document.addEventListener("DOMContentLoaded", function() {
             console.error(error);
         }
     }
+    
+    // Render pokemon
+    async function getPokemon(query) {
+        const pokemon = await fetchData(query);
 
-    // Return Pokemon after user input
+        if (!pokemon) return;
+
+        currentId = pokemon.id;
+
+        //Get stats
+        pokedexNo.textContent = `#${String(pokemon.id).padStart(3, "0")}`;
+        name.textContent = pokemon.name;
+
+        hp.textContent = pokemon.stats[0].base_stat;
+        attack.textContent = pokemon.stats[1].base_stat;
+        defense.textContent = pokemon.stats[2].base_stat;
+        spAttack.textContent = pokemon.stats[3].base_stat;
+        spDefense.textContent = pokemon.stats[4].base_stat;
+        speed.textContent = pokemon.stats[5].base_stat;
+
+        // Get types
+        type1Icon.src = `images/types/${pokemon.types[0].type.name}IC_XY.png`;
+
+        type2Icon.style.display = "inline-block";
+        if (pokemon.types[1]) {
+            type2Icon.src = `images/types/${pokemon.types[1].type.name}IC_XY.png`;
+        }
+        else {
+            type2Icon.style.display = "none";
+        }
+
+        // Get sprites
+        pokemonSprite.src = pokemon.sprites.other.showdown.front_default;
+
+        // Get cries
+        pokemonCry.src = pokemon.cries.latest;
+        pokemonCry.currentTime = 0;
+        pokemonCry.volume = 0.1;
+        pokemonCry.play();
+    }
+
+    // Return Pokemon after user search
     form.addEventListener("submit", async function(event) {
         event.preventDefault();
 
         const query = input.value.trim().toLowerCase();
-
-        const pokemon = await getPokemon(query);
-
-        if (pokemon) {
-
-            // Return text content for HTML
-
-            // Get stats
-            pokedexNo.textContent = `#${String(pokemon.id).padStart(3, "0")}`;
-            name.textContent = pokemon.name;
-            hp.textContent = pokemon.stats[0].base_stat;
-            attack.textContent = pokemon.stats[1].base_stat;
-            defense.textContent = pokemon.stats[2].base_stat;
-            spAttack.textContent = pokemon.stats[3].base_stat;
-            spDefense.textContent = pokemon.stats[4].base_stat;
-            speed.textContent = pokemon.stats[5].base_stat;
-
-            // Get types
-            type1Icon.src = `images/types/${pokemon.types[0].type.name}IC_DPPt.png`;
-
-            if (pokemon.types[1]) {
-                type2Icon.src = `images/types/${pokemon.types[1].type.name}IC_DPPt.png`;
-            }
-            else {
-                type2Icon.style.display = "none";
-            }
-
-            // Get sprites
-            pokemonSprite.src = pokemon.sprites.other.showdown.front_default;
-
-            // Get cries
-            pokemonCry.src = pokemon.cries.legacy;
-            pokemonCry.currentTime = 0;
-            pokemonCry.volume = 0.1;
-            pokemonCry.play();
-        }
+        await getPokemon(query);
     });
 
     // Reset button
@@ -90,7 +97,22 @@ document.addEventListener("DOMContentLoaded", function() {
         }, 100);
     });
 
-    // Keydown
+    // Previous/Next button functionality
+    previous.addEventListener("click", async function() {
+        if (!currentId || currentId <= 1)
+            return;
+
+        await getPokemon(currentId - 1);
+    });
+
+    next.addEventListener("click", async function() {
+        if (!currentId || currentId >= MAX_POKEMON)
+            return;
+
+        await getPokemon(currentId + 1);
+    });
+
+    // Link to arrow keys
     document.addEventListener("keydown", function(event) {
         if (event.key === "ArrowLeft") {
             previous.classList.add("pressed");
@@ -103,7 +125,6 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    // Keyup
     document.addEventListener("keyup", function(event) {
         if (event.key === "ArrowLeft") {
             previous.classList.remove("pressed");
@@ -115,6 +136,4 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-// Add functionality to prev + next buttons
 // Fix search box error handling for invalid pokemon query
-// Fix edge cases - fairy types, weird characters in names
