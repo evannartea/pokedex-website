990c31c73e6d9cef358168dd569720a3ef59d6e8
Evan Nartea <evannartea@Mac.lan>
2026-07-07 12:00:42 +0100

    change to double quotes

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('search-wrapper');
-    const input = document.getElementById('search-box');
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("search-wrapper");
+    const input = document.getElementById("search-box");
 
-    const reset = document.getElementById('reset-button');
-    const previous = document.getElementById('prev-button');
-    const next = document.getElementById('next-button');
+    const reset = document.getElementById("reset-button");
+    const previous = document.getElementById("prev-button");
+    const next = document.getElementById("next-button");
 
-    const pokedexNo = document.getElementById('pokedex-number');
-    const name = document.getElementById('name');
-    const hp = document.getElementById('hp');
-    const attack = document.getElementById('attack');
-    const defense = document.getElementById('defense');
-    const spAttack = document.getElementById('sp-attack');
-    const spDefense = document.getElementById('sp-defense');
-    const speed = document.getElementById('speed');
+    const pokedexNo = document.getElementById("pokedex-number");
+    const name = document.getElementById("name");
+    const hp = document.getElementById("hp");
+    const attack = document.getElementById("attack");
+    const defense = document.getElementById("defense");
+    const spAttack = document.getElementById("sp-attack");
+    const spDefense = document.getElementById("sp-defense");
+    const speed = document.getElementById("speed");
 
-    const type1Icon = document.getElementById('type1');
-    const type2Icon = document.getElementById('type2');
-    const pokemonSprite = document.getElementById('pokemon-sprite');
-    const pokemonCry = document.getElementById('pokemon-cry');
+    const type1Icon = document.getElementById("type1");
+    const type2Icon = document.getElementById("type2");
+    const pokemonSprite = document.getElementById("pokemon-sprite");
+    const pokemonCry = document.getElementById("pokemon-cry");
 
-    const bgMusic = document.getElementById('background-music');
+    const bgMusic = document.getElementById("background-music");
 
     let currentId = null;
     const MAX_POKEMON = 1025;
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
             // Check response is ok
             if(!response.ok) {
-                throw new Error('Could not fetch resource');
+                throw new Error("Could not fetch resource");
             }
             const data = await response.json();
             return data;
@@ -54,7 +54,7 @@ document.addEventListener('DOMContentLoaded', function() {
         currentId = pokemon.id;
 
         //Get stats
-        pokedexNo.textContent = `#${String(pokemon.id).padStart(3, '0')}`;
+        pokedexNo.textContent = `#${String(pokemon.id).padStart(3, "0")}`;
         name.textContent = pokemon.name;
 
         hp.textContent = pokemon.stats[0].base_stat;
@@ -67,12 +67,12 @@ document.addEventListener('DOMContentLoaded', function() {
         // Get types
         type1Icon.src = `images/types/${pokemon.types[0].type.name}IC_XY.png`;
 
-        type2Icon.style.display = 'inline-block';
+        type2Icon.style.display = "inline-block";
         if (pokemon.types[1]) {
             type2Icon.src = `images/types/${pokemon.types[1].type.name}IC_XY.png`;
         }
         else {
-            type2Icon.style.display = 'none';
+            type2Icon.style.display = "none";
         }
 
         // Get sprites
@@ -93,31 +93,31 @@ document.addEventListener('DOMContentLoaded', function() {
         bgMusic.pause();
         bgMusic.currentTime = 0;
 
-        pokedexNo.textContent= '';
-        name.textContent = 'KING OF POP';
-        hp.textContent = 'MAX'
-        attack.textContent = 'MAX'
-        defense.textContent = 'MAX'
-        spAttack.textContent = 'MAX'
-        spDefense.textContent = 'MAX'
-        speed.textContent = 'MAX'
+        pokedexNo.textContent= "";
+        name.textContent = "KING OF POP";
+        hp.textContent = "MAX"
+        attack.textContent = "MAX"
+        defense.textContent = "MAX"
+        spAttack.textContent = "MAX"
+        spDefense.textContent = "MAX"
+        speed.textContent = "MAX"
 
-        type1Icon.src = 'images/types/UnknownIC_Colo.png';
-        type2Icon.style.display = 'none';
+        type1Icon.src = "images/types/UnknownIC_Colo.png";
+        type2Icon.style.display = "none";
 
-        pokemonSprite.src = 'images/michael-jackson.gif';
+        pokemonSprite.src = "images/michael-jackson.gif";
         
-        pokemonCry.src = 'audio/8bit_smooth_criminal.mp3';
+        pokemonCry.src = "audio/8bit_smooth_criminal.mp3";
         pokemonCry.play()
     }
 
     // Return Pokemon after user input
-    form.addEventListener('submit', async function(event) {
+    form.addEventListener("submit", async function(event) {
         event.preventDefault();
 
         const query = input.value.trim().toLowerCase();
 
-        if (query === 'michael jackson') {
+        if (query === "michael jackson") {
             easterEgg();
         }
         else {
@@ -126,21 +126,21 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Reset button
-    reset.addEventListener('click', function() {
+    reset.addEventListener("click", function() {
         setTimeout(function() {
             location.reload();
         }, 100);
     });
 
     // Previous/Next button functionality
-    previous.addEventListener('click', async function() {
+    previous.addEventListener("click", async function() {
         if (!currentId || currentId <= 1)
             return;
 
         await getPokemon(currentId - 1);
     });
 
-    next.addEventListener('click', async function() {
+    next.addEventListener("click", async function() {
         if (!currentId || currentId >= MAX_POKEMON)
             return;
 
@@ -148,25 +148,25 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Link to arrow keys
-    document.addEventListener('keydown', function(event) {
-        if (event.key === 'ArrowLeft') {
-            previous.classList.add('pressed');
+    document.addEventListener("keydown", function(event) {
+        if (event.key === "ArrowLeft") {
+            previous.classList.add("pressed");
             previous.click();
         }
 
-        if (event.key === 'ArrowRight') {
-            next.classList.add('pressed');
+        if (event.key === "ArrowRight") {
+            next.classList.add("pressed");
             next.click();
         }
     });
 
-    document.addEventListener('keyup', function(event) {
-        if (event.key === 'ArrowLeft') {
-            previous.classList.remove('pressed');
+    document.addEventListener("keyup", function(event) {
+        if (event.key === "ArrowLeft") {
+            previous.classList.remove("pressed");
         }
 
-        if (event.key === 'ArrowRight') {
-            next.classList.remove('pressed');
+        if (event.key === "ArrowRight") {
+            next.classList.remove("pressed");
         }
     });
 });
